36775c1ed1d501e8ddb4bd54f5c897159cd96f41
Kevin Stefanelli <50336969+kevstfnl@users.noreply.github.com>
2025-06-13 12:10:05 +0200

    little fix

--- src/components/inputs/Select.jsx
+++ src/components/inputs/Select.jsx
@@ -1,7 +1,7 @@
 
-export default function Select({options, name, id, onChange}) {
+export default function Select({className, options, name, id, onChange}) {
     return (
-        <select name={name} id={id} onChange={onChange}>
+        <select name={name} id={id} onChange={onChange} className={"" + className}>
             {options.map((option, i) => (
                 <option key={i} value={option.value}>{option.name}</option>
             ))}
--- src/pages/authentication/SignInPage.jsx
+++ src/pages/authentication/SignInPage.jsx
@@ -1,29 +1,38 @@
 import Button from "@components/Button";
 import Input from "@components/Input";
 import SignUpFormTitle from "@components/SignUpFormTitle";
-import { NavLink } from "react-router";
+import {NavLink} from "react-router";
+import {processLogin} from "@api/users/authentication.js";
 
 export default function SignInPage() {
-  return (
-      <main className={"h-screen flex justify-center items-center"}>
-        <form className="gap-4 flex flex-col w-4/5 border border-gray-300 p-8 rounded-lg max-w-3xl">
-          <SignUpFormTitle label={"CONNEXION"} />
 
-          <Input
-              htmlFor={"email"}
-              label={"Email"}
-              name={"email"}
-              placeholder={"Entrez votre mail"}
-          />
-          <Input
-              htmlFor={"password"}
-              name={'password'}
-              label={"Mot de passe"}
-              placeholder={"Entrez votre mot de passe"}
-          />
-            <Button className="bg-cyan-500 text-white">Connexion</Button>
-            <NavLink  to={"/signup"} label="S'inscrire" className={`w-[100%] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 bg-orange-100 text-black text-center`}>Pas encore inscrit ?</NavLink>
-        </form>
-      </main>
-  );
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await processLogin(e.data);
+    }
+
+    return (
+        <main className={"h-screen flex justify-center items-center"}>
+            <form className="gap-4 flex flex-col w-4/5 border border-gray-300 p-8 rounded-lg max-w-3xl" onSubmit={handleSubmit}>
+                <SignUpFormTitle label={"CONNEXION"}/>
+
+                <Input
+                    htmlFor={"email"}
+                    label={"Email"}
+                    name={"email"}
+                    placeholder={"Entrez votre mail"}
+                />
+                <Input
+                    htmlFor={"password"}
+                    name={'password'}
+                    label={"Mot de passe"}
+                    placeholder={"Entrez votre mot de passe"}
+                />
+                <Button className="bg-cyan-500 text-white">Connexion</Button>
+                <NavLink to={"/signup"} label="S'inscrire"
+                         className={`w-[100%] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 bg-orange-100 text-black text-center`}>Pas
+                    encore inscrit ?</NavLink>
+            </form>
+        </main>
+    );
 }
