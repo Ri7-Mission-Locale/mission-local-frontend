b9e2046d6b6370a970d7292d93b35378bba856d7
Kevin Stefanelli <50336969+kevstfnl@users.noreply.github.com>
2025-06-25 21:28:31 +0200

    Starting new callendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdminDashboardPage from "@pages/admin/AdminDashboardPage.jsx";
 import UserManagementPage from "@pages/admin/UserManagementPage.jsx";
@@ -8,41 +8,40 @@ import NewsDetail from "@pages/news/NewsDetail.jsx";
 import NewsAdd from "@pages/news/NewsAdd.jsx";
 import LoginPage from "@pages/authentication/LoginPage.jsx";
 import RegisterPage from "@pages/authentication/RegisterPage.jsx";
+import HomePage from "@pages/home/HomePage.jsx";
 
 const client = new QueryClient();
 
 function App() {
-    return (
-        <QueryClientProvider client={client}>
-            <BrowserRouter>
-                <Routes>
+	return (
+		<QueryClientProvider client={client}>
+			<BrowserRouter>
+				<Routes>
+					{/* Default Route TODO home */}
+					<Route path="/" element={<HomePage />} />
 
-                    {/* Authentication routes */}
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
+					{/* Authentication routes */}
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/register" element={<RegisterPage />} />
 
+					{/* Admin Routes */}
+					<Route path="/admin" element={<AdminDashboardPage />} />
+					<Route
+						path="/admin/users"
+						element={<UserManagementPage />}
+					/>
 
-                    {/* Admin Routes */}
-                    <Route path="/admin" element={<AdminDashboardPage />} />
-                    <Route path="/admin/users" element={<UserManagementPage />} />
+					{/* User Routes */}
+					<Route path="/user/profil" element={<UserProfil />} />
 
-                    {/* User Routes */}
-                    <Route path="/user/profil" element={<UserProfil />} />
-
-                    {/* News Routes */}
-                    <Route path="/news/list" element={<NewsList />} />
-                    <Route path="/news/detail/:id" element={<NewsDetail />} />
-                    <Route path="/news/add" element={<NewsAdd />} />
-
-
-                    {/* Default Route TODO home */}
-                    <Route path="/" element={<Navigate to="/login" replace />} />
-                    <Route path="*" element={<Navigate to="/login" replace />} />
-
-                </Routes>
-            </BrowserRouter>
-        </QueryClientProvider>
-    );
+					{/* News Routes */}
+					<Route path="/news/list" element={<NewsList />} />
+					<Route path="/news/detail/:id" element={<NewsDetail />} />
+					<Route path="/news/add" element={<NewsAdd />} />
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
+	);
 }
 
 export default App;
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import Agenda from "@components/Agenda"
+
+export default function HomePage() {
+    return (
+        <>
+            <h1>Welcome to Mission Local!</h1>
+            
+            <section>
+           		<Agenda />
+            </section>
+        </>
+    )
+}
