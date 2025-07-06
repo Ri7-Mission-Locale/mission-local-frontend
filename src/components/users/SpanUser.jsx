0f123c94cad54bb12e1aa3f3489e7d6ce90184df
kevstfnl <seventimepro@gmail.com>
2025-07-06 11:40:39 +0200

    fix UserManagement

--- src/components/users/SpanUser.jsx
+++ src/components/users/SpanUser.jsx
@@ -4,10 +4,10 @@ export default function SpanUser({ user }) {
     return (
         <a className={"flex justify-between items-center gap-3 h-22 p-2 border-t-1 border-gray-400"}>
             <div className="h-full aspect-square rounded-full">
-                <img src="" alt={`Photo de ${user.firstName} ${user.lastName}`} className="bg-amber-600 rounded-full h-full aspect-square" draggable={false} />
+                <img src="https://placehold.co/256" alt={`Photo de ${user.first_name} ${user.last_name}`} className="bg-amber-600 rounded-full h-full aspect-square" draggable={false} />
             </div>
             <div className="w-full flex flex-col">
-                <p>{user.firstName} {user.lastName}</p>
+                <p>{user.first_name} {user.last_name}</p>
                 <p className="text-gray-700">{user.role}</p>
             </div>
             <span className="text-3xl">→</span>
